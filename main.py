5717ec9cd1fc7792270452fe319cc06ddfaa2643
akbarhadiq <97658855+akbarhadiq@users.noreply.github.com>
2022-10-20 14:36:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import smtplib
+import pandas
+import random
+from dotenv import load_dotenv
+import os
+import datetime as dt
+
+# Load .Environment Variable ----------------
+load_dotenv()
+my_email = os.getenv("MY_EMAIL")
+smtp = os.getenv("SMTP")
+password = os.getenv("PASSWORD")
+port = int(os.getenv("PORT"))
+# ------------------------------------------
+
+
+# Get today's date
+current_date = dt.datetime.now()
+current_day = current_date.day
+current_month = current_date.month
+
+
+# Birthday Text Writer
+def writer(name):
+    # randomly chose a letter from the three letters available
+    chosen_letter = random.choice(letter)
+
+    # open the birthday wish text and turn it into a variable
+    with open(f'letter_templates/{chosen_letter}', mode="r") as file:
+        text = file.readlines()
+        # print(text)
+
+    # remove the text /n space
+    bday_wish_text = ''.join(text)
+    # replace default [NAME] with actual name
+    bday_wish_text_with_name = bday_wish_text.replace('[NAME]', name)
+    print(bday_wish_text_with_name)
+    return bday_wish_text_with_name
+
+
+letter = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+df = pandas.read_csv("birthdays.csv")
+birthdays_data = pandas.DataFrame.to_dict(df, orient="records")  # remember orient records!!!!
+
+print(birthdays_data)
+# print(type(birthdays_data[0]["email"]))
+
+
+# checks today date (day, month) with birthday_data
+for birthday in birthdays_data:
+    if birthday["day"] == current_day and birthday["month"] == current_month:
+
+        # get the birthday text
+        birthday_text = writer(birthday['name'])
+
+        # send those wishes
+        connection = smtplib.SMTP(smtp, port)
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday['email'],
+            msg=f"Subject:Happy Birthday!\n\n{birthday_text}"
+        )
+        connection.close()
